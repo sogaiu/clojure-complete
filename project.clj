f2d2f32c833f24dee6784decdc2646b8802c5698
sogaiu <983021772@users.noreply.github.com>
2019-01-14 15:52:10 +0900

    Initial work on CLR support

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/ninjudd/clojure-complete"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {
-    :1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
-    :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
+    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+    :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+    :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
